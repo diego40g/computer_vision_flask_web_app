1ad2b56ba4c0947c4a8998721992cc202af0d0cf
diego40g <diegopaznaula@hotmail.com>
2022-06-14 14:33:12 -0500

    ninja loop for manage json

---
+++ flask-examples/Jinja/3._for_template/main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import views
+
+app = Flask(__name__)
+
+#create rule (uri)
+app.add_url_rule('/','index',views.index)
+
+app.add_url_rule('/template/<name>/<int:age>','index_template',views.index_tem)
+app.add_url_rule('/forloop','index_for',views.index_for)
+
+#run flask app
+if __name__=="__main__":
+    app.run(debug=True, port=3000)
